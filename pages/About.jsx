9381a8d9d278cd47e99ba6cec3c94ab43530884f
Arira-re <reukiowequ723@gmail.com>
2025-11-20 12:11:30 +0900

    test1

--- pages/About.jsx
+++ pages/About.jsx
@@ -13,6 +13,9 @@ export default function About() {
                 <a target="_blank" rel="noopener noreferrer" href="https://booth.pm/ja/items/1515902">ディゴル</a>
                 でいることが多いです。
             </p>
+
+
+            
             <p>基本的には、電子機器系技術を多く知りたいと思っているので、話題としてだしてくれると嬉しいです。</p>
             <p>手を出さないで決めつけるのは簡単。でもそれでものを語るのは危険。触れなければ理解できないこともあることを忘れてはいけない。</p>
             <h2 className="sm:text-3xl sm:leading-10">このサイトについて</h2>
